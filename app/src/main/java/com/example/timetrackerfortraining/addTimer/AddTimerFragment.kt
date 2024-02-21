57df7efeed82412da45d5445895edd78ce6284c1
AlexZenevich <zenevich.job@gmail.com>
2024-02-22 02:33:45 +0300

    add add_timer ui

--- app/src/main/java/com/example/timetrackerfortraining/addTimer/AddTimerFragment.kt
+++ app/src/main/java/com/example/timetrackerfortraining/addTimer/AddTimerFragment.kt
@@ -6,27 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.timetrackerfortraining.R
+import com.example.timetrackerfortraining.databinding.FragmentAddTimerBinding
 
 class AddTimerFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = AddTimerFragment()
-    }
+    private lateinit var binding: FragmentAddTimerBinding
 
     private lateinit var viewModel: AddTimerViewModel
-
+    val firstCategoryList = listOf("Work","Personal","Other")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_timer, container, false)
+        binding = FragmentAddTimerBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddTimerViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, firstCategoryList)
+        binding.firstCategoryInput.setAdapter(adapter)
     }
 
 }
